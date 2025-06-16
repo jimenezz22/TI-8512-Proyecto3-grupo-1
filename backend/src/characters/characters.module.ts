5e98080a4b06f5ea2d7f2696146d2801e5778d27
jimenezz22 <ljimenezbogantes@gmail.com>
2025-06-15 22:41:38 -0600

    [FEAT] Update character and movie modules to include mutual entity imports

--- backend/src/characters/characters.module.ts
+++ backend/src/characters/characters.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Character } from './entities/character.entity';
+import { Movie } from '../movies/entities/movie.entity';
 import { CharactersController } from './characters.controller';
 import { CharactersService } from './characters.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Character])],
+  imports: [TypeOrmModule.forFeature([Character, Movie])],
   controllers: [CharactersController],
   providers: [CharactersService],
   exports: [CharactersService],
--- backend/src/movies/movies.module.ts
+++ backend/src/movies/movies.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Movie } from './entities/movie.entity';
+import { Character } from '../characters/entities/character.entity';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Movie])],
+  imports: [TypeOrmModule.forFeature([Movie, Character])],
   controllers: [MoviesController],
   providers: [MoviesService],
   exports: [MoviesService],
